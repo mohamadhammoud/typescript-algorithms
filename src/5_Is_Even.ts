768271a96536210062c3273d62050fb9a7d4c585
mohamadhammoud <mohamad.hammoudk@gmail.com>
2023-08-25 15:51:40 +0300

    ✨ added is even getter

---
+++ src/5_Is_Even.ts
@@ -0,0 +1,7 @@
+export function isEven(number: number): boolean {
+  return number % 2 === 0; // 1
+}
+// One Case: O(1)
+
+console.log(isEven(2));
+console.log(isEven(11));
--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ import("./1_Linear_vs_Constant_Complexity");
 import("./2_Fibonacci");
 import("./3_Is_Prime");
 import("./4_Get_Minimum");
+import("./5_Is_Even");
